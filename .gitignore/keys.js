ac125a9bda0e55a0ecd5b7c2691a56765050151b
Robert Prine <robertprine2@yahoo.com>
2016-06-03 00:24:50 -0400

    initial commit with api keys for twitter

---
+++ .gitignore/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'mzPj9LmK1LA6NYjx7C1b0WE4L',
+  consumer_secret: 'TPGl5aRhReDAEps8riIUn9qux1HQ4DPwUEETaLd5uIXlGztdGb',
+  access_token_key: '565841137-PvVQ9zFIGrkaQb5FzBKqoRk6A0nrj6TXKN6Iv2cu',
+  access_token_secret: 'n0CgbzfiFc5W3DEpvWIlhe6La5PyT62HSNScE5HVINvu4',
+}
